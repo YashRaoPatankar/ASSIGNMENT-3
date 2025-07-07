1999ba37fed29f53a7acba7905fc005b7678454b
Yash Rao Patankar <75780922+YashRaoPatankar@users.noreply.github.com>
2025-07-07 18:10:24 +0530

    Add files via upload

---
+++ Math.py
@@ -0,0 +1,9 @@
+# for Square root of the number natural logarithm and sine of the number
+import math
+n = float(input("Enter a number: "))
+square_root = math.sqrt(n)
+print(f"Square root = {square_root}")
+natural_log = math.log(n)
+print(f" log = {natural_log}")
+sine_value = math.sin(n)
+print(f" sine = {sine_value}")
